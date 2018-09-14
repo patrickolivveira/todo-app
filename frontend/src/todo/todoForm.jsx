8c4332ef2bddc006ee1fc5e35b1b3464fb896f32
Patrick Mendonça Oliveira <oliveira.patrick94@gmail.com>
2018-09-14 19:03:38 -0300

    integrando com backend

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role="form" className="todoForm">
-        <div className="col-xs-12 col-sm-9 col-md-10">
-            <input id="description" type="text" className="form-control" placeholder="Adicione uma tarefa"/>            
-        </div>
+        <Grid cols='12 9 10'>
+            <input id="description" value={props.description} onChange={props.handleChange} type="text" className="form-control" placeholder="Adicione uma tarefa"/>            
+        </Grid>
 
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary">
-                <i className="fa fa-plus"></i>    
-            </button>    
-        </div>        
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+        </Grid>
     </div>
 )
